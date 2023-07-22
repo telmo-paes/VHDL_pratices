dd7b0cecfc68dfcfb1e377c8072e1042719778d5
telmo-paes <68719049+telmo-paes@users.noreply.github.com>
2023-07-22 16:10:13 -0300

    Update BCD7Seg.vhd

--- BCD7Seg.vhd
+++ BCD7Seg.vhd
@@ -1,3 +1,21 @@
 ---
 ---
 ---
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+entity bcd7seg is port
+  (
+    input_4bits in     :    std_logic_vector(3 downto 0);
+    output_7seg out    :    std_logic_vector(3 downto 0)
+  )
+end bcd7seg;
+
+architecture hardware of bcd7seg is
+begin
+  process(input_4bits,output_7seg)
+    begin
+      case input_4bits is
+        when "0000" => output_7seg <= "11000000"
+        when "0001" => output
